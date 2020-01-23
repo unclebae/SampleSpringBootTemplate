9539993842fb3c3e1dbc0b4d2d8c0e5d7492c474
SKT.배기도 <skt.1111489@sk.com>
2020-01-23 14:20:12 +0900

    modify key value

--- src/main/java/com/template/coe/demo/controller/DynamicHandlerController.java
+++ src/main/java/com/template/coe/demo/controller/DynamicHandlerController.java
@@ -1,60 +1,78 @@
 package com.template.coe.demo.controller;
 
 import com.template.coe.demo.model.DynamicHandler;
-import com.template.coe.demo.service.DynamicHandlerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class DynamicHandlerController {
 
-    @Autowired
-    DynamicHandlerService dynamicHandlerService;
+    private Map<String, DynamicHandler> handlerMaps = new HashMap<>();
 
     @Autowired
     private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     @PostMapping("/api/add-handler")
-    public String addHandler(@RequestBody DynamicHandler handler) throws Exception {
-        return registHandler(handler);
+    public ResponseEntity<DynamicHandler> addHandler(@RequestBody DynamicHandler handler) throws Exception {
+        return ResponseEntity.ok(registHandler(handler));
     }
 
-    public String registHandler(DynamicHandler handler) {
+    @GetMapping("/api/handlers")
+    public ResponseEntity<Map<String, DynamicHandler>> getHandlers() {
+        return ResponseEntity.ok(handlerMaps);
+    }
+
+    @GetMapping("/api/handlers/{key}")
+    public ResponseEntity<DynamicHandler> getHandlersByKey(@PathVariable String key) {
+        return ResponseEntity.ok(handlerMaps.get(key));
+    }
+
+    public DynamicHandler registHandler(DynamicHandler handler) {
         try {
             System.out.println("Handler: " + handler);
             RequestMappingInfo requestMappingInfo = RequestMappingInfo
-                    .paths(handler.getKey() + handler.getUri())
+                    .paths(handler.getUri())
                     .methods(handler.getMethod())
                     .produces(MediaType.APPLICATION_JSON_VALUE)
                     .build();
 
             requestMappingHandlerMapping.registerMapping(requestMappingInfo, this,
-                    DynamicHandlerController.class.getDeclaredMethod("handlerResponse", DynamicHandler.class));
+                    DynamicHandlerController.class.getDeclaredMethod("handlerResponse", HttpServletRequest.class));
 
             System.out.println(requestMappingHandlerMapping.getHandlerMethods());
-            return handler.getKey() + handler.getUri();
+
+            DynamicHandler dynamicHandler = handlerMaps.putIfAbsent(handler.getKey(), handler);
+
+            return dynamicHandler;
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         } catch (SecurityException e) {
             e.printStackTrace();
         }
 
-        return "Error";
+        return null;
     }
 
-    public ResponseEntity<Map<String, Object>> handlerResponse(DynamicHandler handler) {
-        System.out.println("Response: " + handler);
-        return ResponseEntity.ok(handler.getOutput());
-//        return ResponseEntity.ok("OK");
+    public ResponseEntity<Map<String, Object>> handlerResponse(HttpServletRequest request) {
+
+        String requestURI = request.getRequestURI();
+        System.out.println("Request URI: " + requestURI);
+
+        DynamicHandler dynamicHandler = handlerMaps.values().stream().filter((item) -> item.getUri().equals(requestURI)).findFirst().orElse(null);
+
+        if (dynamicHandler == null) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(dynamicHandler.getOutput());
+
     }
 
 }
