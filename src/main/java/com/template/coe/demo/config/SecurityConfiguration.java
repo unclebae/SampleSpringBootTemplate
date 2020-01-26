78f52e7993cf1cfc1ad235c57276ce85f4b5a5ba
iMac <imac@iMacui-iMac.local>
2020-01-27 00:02:39 +0900

    add inMemoryAuthentication

---
+++ src/main/java/com/template/coe/demo/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.template.coe.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kido")
+                .password("kido123")
+                .roles("USER");
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
