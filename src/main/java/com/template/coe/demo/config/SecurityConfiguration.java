8c76702a00b668465780911c2515c98fd92673d5
iMac <imac@iMacui-iMac.local>
2020-01-27 00:12:22 +0900

    add inMemoryAuthentication

--- src/main/java/com/template/coe/demo/config/SecurityConfiguration.java
+++ src/main/java/com/template/coe/demo/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.template.coe.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,13 +16,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("kido")
                 .password("kido123")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                .withUser("kido2")
+                .password("kido2123")
+                .roles("ADMIN");
+
 
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "/aADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 
 }
