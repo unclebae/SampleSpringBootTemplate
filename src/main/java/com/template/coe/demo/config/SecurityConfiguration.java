d7e3dc94d2898159d7fd923590f6aff3219e4f6e
iMac <imac@iMacui-iMac.local>
2020-01-27 10:16:05 +0900

    add spring security with jdbc

--- src/main/java/com/template/coe/demo/config/SecurityConfiguration.java
+++ src/main/java/com/template/coe/demo/config/SecurityConfiguration.java
@@ -1,36 +1,61 @@
 package com.template.coe.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("kido")
-                .password("kido123")
-                .roles("USER")
-                .and()
-                .withUser("kido2")
-                .password("kido2123")
-                .roles("ADMIN");
-
+        // Using query user schema
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+        .usersByUsernameQuery("select username, password, enabled from users where username = ?")
+        .authoritiesByUsernameQuery("select username, authority from authorities where username = ?");
 
     }
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        // Using default user schema
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                ;
+//        ;
+//    }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .withDefaultSchema()
+//                .withUser(
+//                        User.withUsername("user")
+//                                .password("pass")
+//                                .roles("USER")
+//                )
+//                .withUser(
+//                        User.withUsername("admin")
+//                                .password("pass")
+//                                .roles("ADMIN")
+//                )
+//        ;
+//
+//
+//    }
 
     /**
      * / : All (unauthenticated)
@@ -50,5 +75,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "static/css", "static/js").permitAll()
                 .and().formLogin();
 
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
