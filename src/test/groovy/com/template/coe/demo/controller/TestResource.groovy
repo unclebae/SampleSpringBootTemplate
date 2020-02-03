0c4005f2d7b0e5a969e43765420c96dcfc4ad885
SKT.배기도 <skt.1111489@sk.com>
2020-02-03 22:36:49 +0900

    add spock test

--- src/test/groovy/com/template/coe/demo/controller/TestResource.groovy
+++ src/test/groovy/com/template/coe/demo/controller/TestResource.groovy
@@ -1,17 +1,23 @@
 package com.template.coe.demo.controller
 
-
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration )
 @AutoConfigureMockMvc
 @WebMvcTest
 class TestResourceTest extends Specification {
+
+    @Autowired
+    WebApplicationContext context
 
     @Autowired
     private MockMvc mockMvc;
